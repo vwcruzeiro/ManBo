0dfa6de280f06b3e13dd421b5a6cb9764c425443
Vinicius Wilian D. Cruzeiro <vwcruzeiro@ufl.edu>
2018-07-25 09:23:08 -0400

    Adapt test

--- test/4w.n2emb.eembeco.g1/run.sh
+++ test/4w.n2emb.eembeco.g1/run.sh
@@ -4,6 +4,7 @@ if [ -z "$TESTmanbo" ]; then
    TESTmanbo="../../manbo"
 fi
 
+# make sure to set OMP_NUM_THREADS properly
 if [ -z "$DO_PARALLEL" ]; then
    export DO_PARALLEL=" "
 fi
@@ -14,16 +15,7 @@ $DO_PARALLEL $TESTmanbo test.in < /dev/null || error
 ../dacdif -r 1.0e-07 test_forces.dat.save test_forces.dat
 ../dacdif -r 1.0e-07 test_positions.xyz.save test_positions.xyz
 ../dacdif -r 1.0e-07 test_properties.dat.save test_properties.dat
-../dacdif test.rst.in.save test.rst.in
+../dacdif -r 1.0e-07 test.rst.in.save test.rst.in
 ../dacdif -r 1.0e-07 test_velocities.dat.save test_velocities.dat
 
 exit 0
-
-
-
-
-
-
-
-
-
