50b1f9829f846c749caf63b58526b095ab3418e3
Vinicius Wilian D. Cruzeiro <vwcruzeiro@ufl.edu>
2018-07-25 08:42:06 -0400

    Adapting first test

---
+++ test/4w.n2emb.eembeco.g1/run.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ -z "$TESTmanbo" ]; then
+   TESTmanbo="../../manbo"
+fi
+
+if [ -z "$DO_PARALLEL" ]; then
+   export DO_PARALLEL=" "
+fi
+
+$DO_PARALLEL $TESTmanbo test.in < /dev/null || error
+
+../dacdif -r 1.0e-07 test.log.save test.log
+../dacdif -r 1.0e-07 test_forces.dat.save test_forces.dat
+../dacdif -r 1.0e-07 test_positions.xyz.save test_positions.xyz
+../dacdif -r 1.0e-07 test_properties.dat.save test_properties.dat
+../dacdif test.rst.in.save test.rst.in
+../dacdif -r 1.0e-07 test_velocities.dat.save test_velocities.dat
+
+exit 0
+
+
+
+
+
+
+
+
+
