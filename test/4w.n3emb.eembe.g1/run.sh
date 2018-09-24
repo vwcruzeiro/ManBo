c231f1f3a1037065ee2922fef1894bd7e0c2e584
Vinicius Wilian D. Cruzeiro <vwcruzeiro@ufl.edu>
2018-09-24 17:59:43 -0400

    Adjust tests affected by the last commit about Chk files

--- test/4w.n3emb.eembe.g1/run.sh
+++ test/4w.n3emb.eembe.g1/run.sh
@@ -18,4 +18,6 @@ $DO_PARALLEL $TESTmanbo test.in < /dev/null || error
 ../dacdif -r 1.0e-07 test.rst.in.save test.rst.in
 ../dacdif -r 1.0e-07 test_velocities.dat.save test_velocities.dat
 
+rm inp*.chk
+
 exit 0
